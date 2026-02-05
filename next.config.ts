33849a34039dbb8b8550e4505bd46306591061a9
Nikhil Sharma <nikhil7679@gmail.com>
2026-02-05 10:29:47 +0530

    Add GitHub Pages config and workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // 1. Tells Next.js to generate static HTML
+  basePath: "/nikhil_sharma", // 2. YOUR REPO NAME (Must match exactly)
+  images: {
+    unoptimized: true, // 3. Required for GitHub Pages (removes server-side image optimization)
+  },
 };
 
 export default nextConfig;
